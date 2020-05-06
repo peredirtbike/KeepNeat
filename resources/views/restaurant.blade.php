25be047768756c5ec43df1e95c02c66190e7b85f
gbonastre <shirazbolgrot@gmail.com>
2020-05-06 18:03:40 +0200

    restaurants pujar fotos

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -15,12 +15,23 @@
                             <td>Nom</td>
                             <td>Descripcio</td>
                             <td>Opinions</td>
+                            <td>Imatges</td>
+
                         </tr>
                         @foreach ($restaurants as $restaurant)
                             <tr>
                                 <td>{{ $restaurant->nom }}</td>
                                 <td>{{ $restaurant->descripcio }}</td>
                                 <td>{{ $restaurant->opinions }}</td>
+                                <td>
+                                    <img src="asset('/uploads/restaurant/{{ $restaurant->nom }}/{{ $restaurant->imatges }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+                                        
+                                    
+                                    <!-- <img src="/uploads/restaurant/{{ $restaurant->imatges }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;"> -->
+                                
+                                </td>
+
+
 
                             </tr>
                         @endforeach
