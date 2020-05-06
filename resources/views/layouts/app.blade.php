41e08de3691483c13461b191166c79d5a38dfd3c
kepabeats <peredirtbike@gmail.com>
2020-05-06 18:54:29 +0200

    vista edita perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,70 +7,83 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'KeepnEat') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ url('assets/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ url('assets/vendor/jquery-sticky/jquery.sticky.js') }}"></script>
+    <script src="{{ url('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/venobox/venobox.min.js') }}"></script>
+    <script src="{{ url('assets/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ url('assets/js/main.js') }}"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> --}}
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/icofont/icofont.min.css') }}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
+    <link href="{{ url('assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
+    <link href="{{ url('assets/css/style.css')}}" rel="stylesheet">
+
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        <header id="header" class="fixed-top d-flex align-items-center" style="top: 0px; background-color: #444444;">
+            <div class="container d-flex align-items-center">
+        
+              <div class="logo mr-auto">
+              <h1 class="text-light"><a href="{{route('home')}}"><span>Keep n' Eat</span></a></h1>
+                <!-- Uncomment below if you prefer to use an image logo -->
+                <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+              </div>
+        
+              <nav class="nav-menu d-none d-lg-block ">
+                <ul>
+                  <li class="active"><a href="#hero">Home</a></li>
+                  <li><a href="#about">Qui som</a></li>
+                  <li><a href="#why-us">Restaurants</a></li>
+                  <li><a href="#contact">Contacte</a></li>
+          @if (Route::has('login'))
+                            @auth
+                            <li class="ml-5 nav-item dropdown">
+                              <a class="nav-link dropdown-toggle ml-4" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name}} </a>
+                              <img  src="/uploads/avatars/{{ Auth::user()->avatar }}" style="width:32px; height:32px; position:absolute; top:5px; border-radius:50%">
+        
+                              <div class="dropdown-menu" style="background-color: darkgray" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" id="{{ Auth::user()->name }}" href="{{ route('perfil') }}">Perfil</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+                              </div>
                             </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+              
+                            {{-- <li class="logout text-center"><a href="{{ url('/logout') }}">Logout</a></li> --}}
+        
+                            @else
+                            <li class="login text-center"><a href="{{ route('login') }}">Login</a></li>
+        
+                                @if (Route::has('register'))
+                                <li class="register text-center"><a href="{{ route('register') }}">Register</a></li>
+                                @endif
+                            @endauth
+                    @endif
+                </ul>
+              </nav><!-- .nav-menu -->
+        
             </div>
-        </nav>
+          </header><!-- End Header -->
 
         <main class="py-4">
             @yield('content')
--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+{{-- 
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
@@ -61,5 +59,128 @@
     
 </div>
 
-<hr>
+<hr> --}}
+@extends('layouts.app')
+
+@section('content')
+<form enctype="multipart/form-data" class="form-horizontal" action="{{ route('update_avatar') }}" role="form" method="POST">
+
+<div class="container" style="margin-top: 5em";>
+    <h1>Perfil de {{$user->name}}</h1>
+  	<hr>
+	<div class="row">
+      <!-- left column -->
+
+      <div class="col-md-3">
+        <div class="text-center">
+            <img name="avatar" accept="image/png, image/jpeg" src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; border-radius:50%; ">
+            <input class="form-control" type="file" name="avatar" accept="image/png, image/jpeg">
+            <h6>Upload a different photo...</h6>
+          
+          
+        </div>
+      </div>
+      
+      <!-- edit form column -->
+      <div class="col-md-9 personal-info">
+        <div class="alert alert-info alert-dismissable">
+          <a class="panel-close close" data-dismiss="alert">×</a> 
+          <i class="fa fa-coffee"></i>
+          This is an <strong>.alert</strong>. Use this to show important messages to the user.
+        </div>
+        <h3>Personal info</h3>
+        
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Nom:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="nameUser" id="nameUser" type="text" value="{{Auth::user()->name}}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Cognoms:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="secondUser" id="secondUser" type="text" value="{{Auth::user()->cognoms}}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">DNI/NIF:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="dniUser" id="dniUser" type="text" value="{{Auth::user()->nif}}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Data de naixement:</label>
+            <div class="col-lg-8">
+                <input class="form-control" name="naixementUser" id="naixementUser" type="date" value="{{Auth::user()->data_naixement}}" id="example-date-input">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Carrer:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="CarrDirUser" id="CarrDirUser" type="text" value="{{Auth::user()->direccions->carrer}}">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Numero:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="NumDirUser" id="DirUser" type="text" value="{{Auth::user()->direccions->numero}}">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Pis:</label>
+            <div class="col-lg-8">
+              <input class="form-control" name="PisDirUser" id="DirUser" type="text" value="{{Auth::user()->direccions->pis}}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Ciutat:</label>
+            <div class="col-lg-8">
+              <div class="ui-select">
+
+                <select class="form-control" name="ciutatDirUser" id="ciutatDirUser">
+                  <option value="{{ Auth::user()->direccions->ciutats->id }}">{{ Auth::user()->direccions->ciutats->nom }}</option> 
+
+                  @foreach ($ciutats as $ciutat)
+                      <option value="{{ $ciutat->id }}">{{ $ciutat->nom }}</option>
+                  @endforeach
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-lg-3 control-label">Pais:</label>
+            <div class="col-lg-8">
+              <div class="ui-select">
+
+                <select class="form-control" name="paisDirUser" id="paisDirUser">
+                    <option value="{{ Auth::user()->direccions->ciutats->paisos->id }}">{{ Auth::user()->direccions->ciutats->paisos->nom }}</option> 
+
+                    @foreach ($paisos as $pais)
+                        <option value="{{ $pais->id }}">{{ $pais->nom }}</option>
+                    @endforeach
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="col-md-3 control-label"></label>
+            <div class="col-md-8">
+                <input type="hidden" name="idUser" id="idUser" value="{{Auth::user()->id}}">
+                <input type="hidden" name="idDir" id="idDir" value="{{Auth::user()->direccions_id}}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+              <input type="submit" class="btn btn-primary" value="Save Changes">
+              <span></span>
+              <input type="reset" class="btn btn-default" value="Cancel">
+            </div>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
 @endsection
